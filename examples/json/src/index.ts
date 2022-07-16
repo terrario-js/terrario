e9240aa0e7269ea4d4f0266aa2b55fa7bb6024be
marihachi <marihachi0620@gmail.com>
2022-07-16 20:39:59 +0900

    json example

--- examples/json/src/index.ts
+++ examples/json/src/index.ts
@@ -2,14 +2,19 @@ import * as P from 'terrario';
 
 // WIP
 
-const spacing = P.regexp(/[ \t\r\n]/);
+const spaces = P.regexp(/[ \t\r\n]*/);
 
 const lang = P.createLanguage({
 	value: r => P.alt([
+		r.null,
 		r.bool,
 		r.string,
+		r.object,
+		r.array,
 		r.number,
 	]),
+
+	null: r => P.str('null'),
 
 	bool: r => P.alt([
 		P.str('true'),
@@ -34,16 +39,65 @@ const lang = P.createLanguage({
 		return parseFloat(value);
 	}),
 
+	object: r => {
+		const entry = P.seq([
+			r.string,
+			spaces,
+			P.str(':'),
+			spaces,
+			r.value,
+		]).map((value: unknown[]) => {
+				return { key: value[0], value: value[4] };
+		});
+		const separator = P.seq([
+			spaces,
+			P.str(','),
+			spaces,
+		]);
+		return P.seq([
+			P.str('{'),
+			spaces,
+			P.option(entry.sep1(separator)),
+			spaces,
+			P.str('}'),
+		], 2).map((value: { key: string, value: unknown }[] | null) => {
+			if (value == null) {
+				return {};
+			}
+			const obj: Record<string, unknown> = {};
+			for (let kvp of value) {
+				obj[kvp.key] = kvp.value;
+			}
+			return obj;
+		});
+	},
+
+	array: r => {
+		const separator = P.seq([
+			spaces,
+			P.str(','),
+			spaces,
+		]);
+		return P.seq([
+			P.str('['),
+			spaces,
+			P.option(r.value.sep1(separator)),
+			spaces,
+			P.str(']'),
+		], 2).map((value: unknown[] | null) => {
+			return (value != null ? value : []);
+		});
+	},
 });
 
 const json = P.seq([
-	spacing.atLeast(0),
+	spaces,
 	lang.value,
-	spacing.atLeast(0),
+	spaces,
 ], 1);
 
 function parseJson(input: string) {
-	const result = json.handler(input, 0, {});
+	const result = json.handler(input, 0, { });
 	if (!result.success || result.index < input.length) {
 		throw new Error('failed to parse JSON.');
 	}
