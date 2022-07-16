01762679a8033892778afb16bec27b9860d307ec
marihachi <marihachi0620@gmail.com>
2022-07-16 20:01:45 +0900

    json example

--- examples/json/src/index.ts
+++ examples/json/src/index.ts
@@ -2,27 +2,48 @@ import * as P from 'terrario';
 
 // WIP
 
-const jBool = P.alt([
-	P.str('true'),
-	P.str('false'),
-]).map((value: 'true' | 'false') => {
-	return (value === 'true');
+const spacing = P.regexp(/[ \t\r\n]/);
+
+const lang = P.createLanguage({
+	value: r => P.alt([
+		r.bool,
+		r.string,
+		r.number,
+	]),
+
+	bool: r => P.alt([
+		P.str('true'),
+		P.str('false'),
+	]).map((value: 'true' | 'false') => {
+		return (value === 'true');
+	}),
+
+	string: r => P.seq([
+		P.str('"'),
+		P.seq([
+			P.notMatch(P.alt([P.str('"'), P.cr, P.lf])),
+			P.any
+		]).atLeast(0).text(),
+		P.str('"'),
+	], 1),
+
+	number: r => P.alt([
+		P.regexp(/[+-]?[0-9]+\.[0-9]+/),
+		P.regexp(/[+-]?[0-9]+/),
+	]).map((value: string) => {
+		return parseFloat(value);
+	}),
+
 });
 
-const jNumber = P.alt([
-	P.regexp(/[+-]?[0-9]+\.[0-9]+/),
-	P.regexp(/[+-]?[0-9]+/),
-]).map((value: string) => {
-	return parseFloat(value);
-});
-
-const jValue = P.alt([
-	jBool,
-	jNumber,
-]);
+const json = P.seq([
+	spacing.atLeast(0),
+	lang.value,
+	spacing.atLeast(0),
+], 1);
 
 function parseJson(input: string) {
-	const result = jValue.handler(input, 0, {});
+	const result = json.handler(input, 0, {});
 	if (!result.success || result.index < input.length) {
 		throw new Error('failed to parse JSON.');
 	}
@@ -35,3 +56,4 @@ console.log(parseJson('123'));
 console.log(parseJson('-123'));
 console.log(parseJson('123.45'));
 console.log(parseJson('-123.45'));
+console.log(parseJson('"abc"'));
