b0eb386bfdb4f0e3ecd127da617ebaf8f3a4eb02
marihachi <marihachi0620@gmail.com>
2022-07-16 15:09:31 +0900

    add test

--- test/index.ts
+++ test/index.ts
@@ -1,11 +1,79 @@
 import assert from 'assert';
 import * as P from '../src/index';
 
+it('any', () => {
+	const input = 'ab';
+	const parser = P.any;
+	const result = parser.handler(input, 0, {});
+	assert.ok(result.success);
+	assert.deepStrictEqual(result.value, 'a');
+	assert.strictEqual(result.index, 1);
+});
+
 it('str', () => {
 	const input = 'abc';
 	const parser = P.str('abc');
 	const result = parser.handler(input, 0, {});
 	assert.ok(result.success);
-	assert.strictEqual(result.value, 'abc');
+	assert.deepStrictEqual(result.value, input);
 	assert.strictEqual(result.index, 3);
+});
+
+it('regexp', () => {
+	const input = 'abcDEF';
+	const parser = P.regexp(/[a-z]+/i);
+	const result = parser.handler(input, 0, {});
+	assert.ok(result.success);
+	assert.deepStrictEqual(result.value, input);
+	assert.strictEqual(result.index, 6);
+});
+
+it('alt', () => {
+	const input = '123';
+	const parser = P.alt([
+		P.str('abc'),
+		P.str('123'),
+	]);
+	const result = parser.handler(input, 0, {});
+	assert.ok(result.success);
+	assert.deepStrictEqual(result.value, input);
+	assert.strictEqual(result.index, 3);
+});
+
+describe('seq', () => {
+	it('basic', () => {
+		const input = 'abc123';
+		const parser = P.seq([
+			P.str('abc'),
+			P.str('123'),
+		]);
+		const result = parser.handler(input, 0, {});
+		assert.ok(result.success);
+		assert.deepStrictEqual(result.value, ['abc', '123']);
+		assert.strictEqual(result.index, 6);
+	});
+
+	it('with parser.text()', () => {
+		const input = 'abc123';
+		const parser = P.seq([
+			P.str('abc'),
+			P.str('123'),
+		]).text();
+		const result = parser.handler(input, 0, {});
+		assert.ok(result.success);
+		assert.deepStrictEqual(result.value, input);
+		assert.strictEqual(result.index, 6);
+	});
+
+	it('with select param', () => {
+		const input = 'abc123';
+		const parser = P.seq([
+			P.str('abc'),
+			P.str('123'),
+		], 0);
+		const result = parser.handler(input, 0, {});
+		assert.ok(result.success);
+		assert.deepStrictEqual(result.value, 'abc');
+		assert.strictEqual(result.index, 6);
+	});
 });
