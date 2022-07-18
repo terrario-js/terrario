a23f047ff7815d59486dfda2b40f7898fb293d20
marihachi <marihachi0620@gmail.com>
2022-07-18 18:18:45 +0900

    check error for createLanguage

--- src/index.ts
+++ src/index.ts
@@ -234,6 +234,9 @@ export function createLanguage<T>(syntaxes: { [K in keyof T]: (r: Record<string,
 	for (const key of Object.keys(syntaxes)) {
 		rules[key] = lazy(() => {
 			const parser = (syntaxes as any)[key](rules);
+			if (parser == null) {
+				throw new Error('syntax must return a parser.');
+			}
 			parser.name = key;
 			return parser;
 		});
