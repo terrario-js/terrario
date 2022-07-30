c661c17b193dbe8a4b501dee4cdadb8dffe28074
marihachi <marihachi0620@gmail.com>
2022-07-30 18:41:49 +0900

    test

--- test/index.ts
+++ test/index.ts
@@ -1,144 +1,175 @@
 import assert from 'assert';
 import * as P from '../src/index';
 
-it('char', () => {
-	const input = 'ab';
-	const parser = P.char;
-	const result = parser.parse(input);
-	assert.ok(result.success);
-	assert.deepStrictEqual(result.value, 'a');
-	assert.strictEqual(result.index, 1);
-});
+describe('Parser', () => {
+	// it('parse()', () => {
+	// });
 
-it('str', () => {
-	const input = 'abc';
-	const parser = P.str('abc');
-	const result = parser.parse(input);
-	assert.ok(result.success);
-	assert.deepStrictEqual(result.value, input);
-	assert.strictEqual(result.index, 3);
-});
+	// it('map()', () => {
+	// });
 
-it('regexp', () => {
-	const input = 'abcDEF';
-	const parser = P.regexp(/[a-z]+/i);
-	const result = parser.parse(input);
-	assert.ok(result.success);
-	assert.deepStrictEqual(result.value, input);
-	assert.strictEqual(result.index, 6);
-});
-
-it('alt', () => {
-	const input = '123';
-	const parser = P.alt([
-		P.str('abc'),
-		P.str('123'),
-	]);
-	const result = parser.parse(input);
-	assert.ok(result.success);
-	assert.deepStrictEqual(result.value, input);
-	assert.strictEqual(result.index, 3);
-});
-
-describe('seq', () => {
-	it('basic', () => {
+	it('text()', () => {
 		const input = 'abc123';
 		const parser = P.seq([
+			P.str('abc'),
+			P.str('123'),
+		]).text();
+		const result = parser.parse(input);
+		assert.ok(result.success);
+		assert.deepStrictEqual(result.value, input);
+		assert.strictEqual(result.index, 6);
+	});
+
+	it('many()', () => {
+		let input, parser, result;
+
+		parser = P.str('abc').many(1);
+
+		input = 'abc123';
+		result = parser.parse(input);
+		assert.ok(result.success);
+		assert.deepStrictEqual(result.value, ['abc']);
+		assert.strictEqual(result.index, 3);
+
+		input = 'abcabc123';
+		result = parser.parse(input);
+		assert.ok(result.success);
+		assert.deepStrictEqual(result.value, ['abc', 'abc']);
+		assert.strictEqual(result.index, 6);
+
+		input = 'ab123';
+		result = parser.parse(input);
+		assert.ok(!result.success);
+	});
+
+	// it('option()', () => {
+	// });
+});
+
+describe('Combinators', () => {
+	it('str()', () => {
+		const input = 'abc';
+		const parser = P.str('abc');
+		const result = parser.parse(input);
+		assert.ok(result.success);
+		assert.deepStrictEqual(result.value, input);
+		assert.strictEqual(result.index, 3);
+	});
+
+	it('regexp()', () => {
+		const input = 'abcDEF';
+		const parser = P.regexp(/[a-z]+/i);
+		const result = parser.parse(input);
+		assert.ok(result.success);
+		assert.deepStrictEqual(result.value, input);
+		assert.strictEqual(result.index, 6);
+	});
+
+	describe('seq()', () => {
+		it('basic', () => {
+			const input = 'abc123';
+			const parser = P.seq([
+				P.str('abc'),
+				P.str('123'),
+			]);
+			const result = parser.parse(input);
+			assert.ok(result.success);
+			assert.deepStrictEqual(result.value, ['abc', '123']);
+			assert.strictEqual(result.index, 6);
+		});
+
+		it('with select param', () => {
+			const input = 'abc123';
+			const parser = P.seq([
+				P.str('abc'),
+				P.str('123'),
+			], 0);
+			const result = parser.parse(input);
+			assert.ok(result.success);
+			assert.deepStrictEqual(result.value, 'abc');
+			assert.strictEqual(result.index, 6);
+		});
+	});
+
+	it('alt()', () => {
+		const input = '123';
+		const parser = P.alt([
 			P.str('abc'),
 			P.str('123'),
 		]);
 		const result = parser.parse(input);
 		assert.ok(result.success);
-		assert.deepStrictEqual(result.value, ['abc', '123']);
-		assert.strictEqual(result.index, 6);
+		assert.deepStrictEqual(result.value, input);
+		assert.strictEqual(result.index, 3);
 	});
 
-	it('with select param', () => {
-		const input = 'abc123';
-		const parser = P.seq([
-			P.str('abc'),
-			P.str('123'),
-		], 0);
+	it('sep()', () => {
+		let input, parser, result;
+
+		parser = P.sep(P.str('abc'), P.str(','), 2);
+
+		input = 'abc,abc';
+		result = parser.parse(input);
+		assert.ok(result.success);
+		assert.deepStrictEqual(result.value, ['abc', 'abc']);
+		assert.strictEqual(result.index, 7);
+
+		input = 'abc';
+		result = parser.parse(input);
+		assert.ok(!result.success);
+	});
+
+	// it('lazy()', () => {
+	// });
+
+	// it('match()', () => {
+	// });
+
+	// it('notMatch()', () => {
+	// });
+
+	it('cond()', () => {
+		let input, parser, result;
+
+		parser = P.seq([
+			P.cond(state => state.enabled),
+			P.char,
+		]);
+
+		result = parser.parse('a', { enabled: true });
+		assert.ok(result.success);
+
+		result = parser.parse('a', { enabled: false });
+		assert.ok(!result.success);
+	});
+
+	it('eof', () => {
+		let input, parser, result;
+
+		parser = P.eof;
+
+		result = parser.parse('');
+		assert.ok(result.success);
+
+		result = parser.parse('a');
+		assert.ok(!result.success);
+	});
+
+	it('char', () => {
+		const input = 'ab';
+		const parser = P.char;
 		const result = parser.parse(input);
 		assert.ok(result.success);
-		assert.deepStrictEqual(result.value, 'abc');
-		assert.strictEqual(result.index, 6);
+		assert.deepStrictEqual(result.value, 'a');
+		assert.strictEqual(result.index, 1);
 	});
+
+	// it('lineBegin()', () => {
+	// });
+
+	// it('lineEnd()', () => {
+	// });
 });
 
-it('parser.text()', () => {
-	const input = 'abc123';
-	const parser = P.seq([
-		P.str('abc'),
-		P.str('123'),
-	]).text();
-	const result = parser.parse(input);
-	assert.ok(result.success);
-	assert.deepStrictEqual(result.value, input);
-	assert.strictEqual(result.index, 6);
-});
-
-it('parser.many()', () => {
-	let input, parser, result;
-
-	parser = P.str('abc').many(1);
-
-	input = 'abc123';
-	result = parser.parse(input);
-	assert.ok(result.success);
-	assert.deepStrictEqual(result.value, ['abc']);
-	assert.strictEqual(result.index, 3);
-
-	input = 'abcabc123';
-	result = parser.parse(input);
-	assert.ok(result.success);
-	assert.deepStrictEqual(result.value, ['abc', 'abc']);
-	assert.strictEqual(result.index, 6);
-
-	input = 'ab123';
-	result = parser.parse(input);
-	assert.ok(!result.success);
-});
-
-it('parser.sep()', () => {
-	let input, parser, result;
-
-	parser = P.sep(P.str('abc'), P.str(','), 2);
-
-	input = 'abc,abc';
-	result = parser.parse(input);
-	assert.ok(result.success);
-	assert.deepStrictEqual(result.value, ['abc', 'abc']);
-	assert.strictEqual(result.index, 7);
-
-	input = 'abc';
-	result = parser.parse(input);
-	assert.ok(!result.success);
-});
-
-it('eof', () => {
-	let input, parser, result;
-
-	parser = P.eof;
-
-	result = parser.parse('');
-	assert.ok(result.success);
-
-	result = parser.parse('a');
-	assert.ok(!result.success);
-});
-
-it('cond', () => {
-	let input, parser, result;
-
-	parser = P.seq([
-		P.cond(state => state.enabled),
-		P.char,
-	]);
-
-	result = parser.parse('a', { enabled: true });
-	assert.ok(result.success);
-
-	result = parser.parse('a', { enabled: false });
-	assert.ok(!result.success);
-});
+// it('createLanguage()', () => {
+// });
